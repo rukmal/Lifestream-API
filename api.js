75828c41d1e9715e2dd9c78c4f805d92e6c84f9d
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2014-10-17 22:39:19 -0700

    Finished endpoint to add a new post and added global error checking

--- api.js
+++ api.js
@@ -1,6 +1,9 @@
 function Api (posts, router) {
+
+	// Add new post
 	router.route('/post/new')
 		.post(function (req, res) {
+			checkHeaders(res, req.body, ['user_id', 'photo', 'latitude', 'longitude', 'alias']);
 			var newPost = {};
 			newPost['location_bucket'] = getLocation(req.body.latitude, req.body.longitude);
 			if (req.body.alias) {
@@ -10,6 +13,15 @@ function Api (posts, router) {
 			newPost['upvotes'] = 0;
 			newPost['downvotes'] = 0;
 			newPost['photo'] = req.body.photo;
+			var newMongoosePost = new posts(newPost);
+			newMongoosePost.save(function (err) {
+				if (err) {
+					console.log(err);
+					res.status(500).end();
+				} else {
+					res.status(200).end();
+				}
+			});
 		});
 
 	/**
@@ -22,6 +34,16 @@ function Api (posts, router) {
 	function getLocation (latitude, longitude) {
 		return 'University of Washington';
 	}
+
+	function checkHeaders (res, reqBody, parameters) {
+		for (var parameterNo in parameters) {
+			var parameter = parameters[parameterNo];
+			if (!reqBody.hasOwnProperty(parameter)) {
+				res.status(500).end();
+			}
+		}
+	}
+
 }
 
 module.exports = Api;
