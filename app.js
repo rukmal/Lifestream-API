504ba9dd60bbca7e41087f17f1b359727ba4e36b
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2014-10-18 02:57:13 -0700

    Added some experimental dropbox stuff, and changed the max request size to allow larger images through

--- app.js
+++ app.js
@@ -21,14 +21,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(logger('dev'));
-app.use(bodyParser.json());
+app.use(bodyParser.json({
+	limit: '50mb'
+}));
 app.use(bodyParser.urlencoded({
+	limit: '50mb',
 	extended: true
 }));
 app.use(methodOverride());
 
 var router = express.Router();
 
+var picture_db = require('./picture-db/dropbox');
 router.get('/', function (req, res) {
 	res.redirect('http://api.ghostfra.me');
 });
---
+++ picture-db/dropbox.js
@@ -0,0 +1,8 @@
+var dropbox = require('dropbox');
+var exports = module.exports;
+
+// connecting to Dropbox
+var client = new dropbox.Client({
+	key: 'juc613g9q45i8sq',
+	secret: 'hqxnlob47bbz2tu'
+});
