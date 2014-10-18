56e74798c33fa51d7a68cbbd5d53f3b973fd3e2c
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2014-10-17 20:20:21 -0700

    Added node+express skeleton

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var bodyParser = require('body-parser');
+var logger = require('morgan');
+var methodOverride = require('method-override');
+
+var app = express();
+
+// all environments
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+app.use(methodOverride());
+
+var router = express.Router();
+
+app.use('/', router);
+var port = process.env.PORT || 3000;
+app.listen(port);
+console.log('Express server listening on port ' + port);
